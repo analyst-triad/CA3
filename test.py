54338b4ee068be518774fd4f81cfe2224e483717
analyst-triad <saadfarooq404@yahoo.com>
2023-09-19 21:50:18 +0500

    fixed test cases

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from app import square, sub, div
+from app import square, sub, div, multiply
 
 def test_square():
-    assert 4 == square(2)
+    assert "4" == square(2)
 def test_sub():
-    assert 1 == sub(4)
+    assert "1" == sub(4)
 def test_div():
-    assert 2 == div(4,2)
+    assert "2" == div(4,2)
+def test_multiply():
+    assert "4" == multiply(2,2)
